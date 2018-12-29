f1815e1316b3bd0f1af46b86c23490dc2ccc7807
Alex Xie <alexieyizhe@gmail.com>
2018-12-29 16:40:49 -0500

    Add base structure

---
+++ src/components/PageFooter.jsx
@@ -0,0 +1,18 @@
+/* --- Packages and Components --- */
+import React from 'react';
+// import styled from 'styled-components';
+
+/* --- Images --- */
+
+class PageFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>footer</div>;
+  }
+}
+
+export default PageFooter;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,48 @@
+/* --- Packages and Components --- */
 import React from 'react';
+import styled from 'styled-components';
+import { isMobile } from 'react-device-detect';
 
-export default () => <div>Hello world!</div>;
+import PageHeader from '../components/PageHeader';
+import PageFooter from '../components/PageFooter';
+
+/* --- Images --- */
+import HeroImg from '../static/img/hero.png';
+
+const MobileMenu = styled.div`
+  color: red;
+`;
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <PageHeader />
+
+        {isMobile ? <MobileMenu>mobile menu</MobileMenu> : null}
+
+        <div className="page hero">
+          Equithon
+          <img src={HeroImg} alt="A person thinking." />
+        </div>
+
+        <div className="page about1">what is equithon?</div>
+
+        <div className="page about2">whats in store?</div>
+
+        <div className="page sponsors">sponsors</div>
+
+        <div className="page faq">faq</div>
+
+        <PageFooter />
+      </div>
+    );
+  }
+}
+
+export default MainPage;
