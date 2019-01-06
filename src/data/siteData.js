1e7a4e7381ba4e451fc559d69fb75f0fb2d033b7
Alex Xie <alexieyizhe@gmail.com>
2019-01-05 23:41:04 -0500

    Add 'What is Equithon?' page (#23)
    * Trim pic and add offset for content
    
    * Minor styling fixes

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -25,7 +25,23 @@ export const mainPageData = {
 };
 
 export const aboutPageData = {
-  mainText: 'Be a part of the change.'
+  header: 'What is Equithon?',
+  desc:
+    'Equithon is a student-run hackathon held at the University of Waterloo where participants create solutions for a variety of social equity challenges. Our goal is to create an inclusive environment where all attendees feel comfortable learning, sharing their ideas, and developing something amazing that can improve the world around us.',
+  testimonials: [
+    {
+      authorName: 'Mandy Meindersma',
+      authorDesc: 'University of Alberta CS, Equithon 2018 Category Winner',
+      authorQuote:
+        'I was able to meet three amazing bad ass women who helped me grow and made me a better person. We all found our own strengths and we made something amazing together.'
+    },
+    {
+      authorName: 'Mandy Meindersma',
+      authorDesc: 'University of Alberta CS, Equithon 2018 Category Winner',
+      authorQuote:
+        'I was able to meet three amazing bad ass women who helped me grow and made me a better person. We all found our own strengths and we made something amazing together.'
+    }
+  ]
 };
 
 export const mobileMenuData = {
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,13 +2,167 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { mediaSize } from '../data/siteTools';
+
+import { aboutPageData } from '../data/siteData';
+
 /* --- Images --- */
+import AboutImg from '../static/img/about_1.png';
 
 /* --- Styles --- */
 const PageContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: grey;
+  margin: 0;
+  background-color: white;
+  box-sizing: border-box;
+`;
+
+const ContentContainer = styled.div`
+  padding-top: 10vh;
+  width: 85vw;
+  height: 80vh;
+  margin: auto;
+
+  color: #46484a;
+
+  display: grid;
+  grid-template-columns: 5fr 4fr;
+  grid-template-rows: 2fr 2fr;
+  grid-template-areas:
+    'pic main'
+    'pic testimonial';
+
+  ${mediaSize.tablet`
+    padding-top: 5vh;
+    width: 80vw;
+    height: 90vh;
+    grid-template-columns: auto;
+    grid-template-rows: 1fr 3fr 1fr;
+    grid-row-gap: 4vw;
+    grid-template-areas:
+      'main'
+      'pic'
+      'testimonial';
+  `};
+
+  ${mediaSize.phone`
+    grid-template-rows: 1fr 3fr 1fr;
+  `};
+`;
+
+const AboutImgContainer = styled.img`
+  position: relative;
+  left: 2.5vw;
+  top: 2em;
+  grid-area: pic;
+  justify-self: end;
+  align-self: center;
+  max-height: 100%;
+  max-width: 100%;
+
+  ${mediaSize.tablet`
+    top: 0; left: 0;
+    justify-self: center;
+  `};
+`;
+
+const MainContainer = styled.div`
+  grid-area: main;
+  align-self: end;
+  margin-bottom: 5vh;
+  right: 2.5vw;
+  position: relative;
+  z-index: 1;
+  top: -2em;
+
+  ${mediaSize.tablet`
+    top: 0; right: 0;
+    justify-self: center;
+    align-self: center;
+    margin-bottom: 0;
+  `};
+
+  ${mediaSize.phone`
+    padding-top: 5%;
+  `};
+`;
+
+const MainHeader = styled.div`
+  font-size: 2.5vw;
+  font-weight: 500;
+  margin-bottom: 10px;
+
+  ${mediaSize.tablet`
+    font-size: 5vw;
+  `};
+
+  ${mediaSize.phone`
+    font-size: 7vw;
+  `};
+`;
+
+const MainDesc = styled.div`
+  font-size: 1.3vw;
+  font-weight: 400;
+
+  ${mediaSize.tablet`
+    font-size: 3vw;
+  `};
+
+  ${mediaSize.phone`
+    font-size: 4vw;
+  `};
+`;
+
+const TestimonialContainer = styled.div`
+  grid-area: testimonial;
+  align-self: start;
+  margin-bottom: 5vh;
+  width: 100%;
+  right: 2.5vw;
+  position: relative;
+  z-index: 1;
+  top: -2em;
+
+  font-size: 1.3vw;
+
+  & > div {
+    color: #9c9797;
+    width: 100%;
+  }
+
+  ${mediaSize.tablet`
+    top: 0; right: 0;
+    justify-self: center;
+    font-size: 3vw;
+    margin-bottom: 0;
+    text-align: center;
+  `};
+
+  ${mediaSize.phone`
+    font-size: 4vw;
+  `};
+`;
+
+const TestimonialQuote = styled.div`
+  margin-bottom: 1em;
+  //text-align: justify;
+
+  ${mediaSize.tablet`
+    text-align: justify;
+  `};
+`;
+
+const TestimonialAuthorName = styled.div`
+  font-style: italic;
+  //text-align: right;
+`;
+
+const TestimonialAuthorDesc = styled.div`
+  font-weight: 300;
+  font-style: italic;
+  //text-align: center;
 `;
 
 /* --- Component --- */
@@ -21,7 +175,26 @@ class AboutPage extends React.Component {
   render() {
     return (
       <PageContainer className="section" id="about">
-        about page
+        <ContentContainer>
+          <AboutImgContainer src={AboutImg} />
+
+          <MainContainer>
+            <MainHeader>{aboutPageData.header}</MainHeader>
+            <MainDesc>{aboutPageData.desc}</MainDesc>
+          </MainContainer>
+
+          <TestimonialContainer>
+            <TestimonialQuote>
+              &quot;{aboutPageData.testimonials[0].authorQuote}&quot;
+            </TestimonialQuote>
+            <TestimonialAuthorName>
+              - {aboutPageData.testimonials[0].authorName}
+            </TestimonialAuthorName>
+            <TestimonialAuthorDesc>
+              {aboutPageData.testimonials[0].authorDesc}
+            </TestimonialAuthorDesc>
+          </TestimonialContainer>
+        </ContentContainer>
       </PageContainer>
     );
   }
